e2e743b0f87ca9961268a62622a303e14c046183
Apple7575 <Apple7575@users.noreply.github.com>
2026-02-11 08:43:22 +0900

    Delete swea-1926.py by 장수철

--- jsc/26-01-w5/swea-1926.py
+++
@@ -1,9 +0,0 @@
-T = 10
-for i in range(1, T+1):
-    for a in str(i):
-        result = ""
-        if a == str(3) or a == str(6) or a == str(9):
-            result += "-"
-        else:
-            result = a
-    print(result, end = " ")
