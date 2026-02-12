1235228a7ec3b7f586349865195ca870a0b092d2
Junch-Lee <Junch-Lee@users.noreply.github.com>
2026-02-12 20:33:06 +0900

    Update swea-4615-v1 by 이창준

--- lcj/26-02-w2/swea-4615-v1.py
+++ lcj/26-02-w2/swea-4615-v1.py
@@ -1,7 +1,6 @@
-# 미완입니다
 T = int(input())
 
-d = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1,0), (1,-1), (0,-1), (-1,-1)]
+d = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1,0), (1,-1), (0,-1), (-1,-1)] # 8방향 탐색 준비
 
 def orthello(arr, move):
 
@@ -10,36 +9,53 @@ def orthello(arr, move):
         r = r_m-1 # 행 이동 위치
         c = c_m-1 # 열 이동 위치
         arr[r][c] = color # 해당 위치에 돌 놓기
-        change_list = []
 
         for dr, dc in d: # 2. 사잇돌 바꾸기 (돌 탐색 w/ 델타)
-            is_change = False
+            is_change = False # 바꾸는 상황 : X로 시작
 
-            for p in range(1, N):
-                nr = r + dr*p   # 행 이동 위치
-                nc = c + dc*p   # 열 이동 위치
+            nr = r + dr # 8방향 
+            nc = c + dc 
+            change_list = []
 
-                if 0 <= nr < N and 0 <= nc < N:  # 옳은 범위 내에서
-                    if arr[nr][nc] != color: # 다른 색을 만나면 -> 계속 탐색
-                        is_change = True
+            if (0 <= nr < N
+                and 0 <= nc < N  
+                and arr[nr][nc] not in (0, color) ): # 2-1. 옳은 범위 내에서 다른 색을 만나면 -> 탐색 시작
+                    
+                    change_list.append((nr,nc)) # 값을 바꿔줄 리스트에 추가
 
-                        change_list.append((nr, nc))
-                        continue
+                    while True: # 2-2.다시 같은 색을 만날 때까지 반복해서 해당 방향으로 전진
+                         
+                         nr += dr # 해당 방향으로 전진
+                         nc += dc 
 
-                    if arr[nr][nc] == color and is_change: # 같은 색을 만나면
+                         if not (0 <= nr < N and 0 <= nc < N): # 2-3. 범위 체크 - 벗어나면 실패
+                           change_list = []
+                           break
+                        
+                         elif arr[nr][nc] == 0: # 2-4. 빈칸을 만나면 - 실패
+                              change_list = []
+                              break
+                               
+                         if arr[nr][nc] == color : # 0을 만나면
+                              break
+                         
+                         change_list.append((nr, nc))
+                    
+                    # 현재 상황 : 다른 색을 만날 때 까지 전진해서 해당 좌표값을 change_list에 넣었음
+                    # 3. 다른 색 돌 바꿔주기
+                    for cr, cc in change_list:
+                         arr[cr][cc] = color
 
-                        for cr, cc in change_list:
-                            arr[cr][cc] = color
+    b_cnt = 0
+    w_cnt = 0
+    for r in range(N):
+         for c in range(N):
+              if arr[r][c] == 1:
+                   b_cnt += 1
+              elif arr[r][c] == 2:
+                   w_cnt += 1
 
-    else: # 게임 종료 이후
-
-        b_cnt = 0
-        for r in range(N):
-            for c in range(N):
-                if arr[r][c] == 1:
-                    b_cnt += 1
-
-        return b_cnt
+    return (b_cnt, w_cnt)
 
 for tc in range(1, T+1):
 
@@ -54,4 +70,4 @@ for tc in range(1, T+1):
 
     result = orthello(arr, move)
 
-    print(f"#{tc} {result} {N**2 - result}")
+    print(f"#{tc}", *result, sep=' ')
