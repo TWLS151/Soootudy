cb1cf8ac61f6d1a8571f81b7699e817f905e2bf9
59raphyy-cloud <59raphyy-cloud@users.noreply.github.com>
2026-02-17 11:33:49 +0900

    Add swea-4828-v1 by 오규연

---
+++ oky/26-02-w3/swea-4828-v1.py
@@ -0,0 +1,10 @@
+# SWEA-4828 min max
+
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    
+    print(f'#{test_case} {max(arr) - min(arr)}')
